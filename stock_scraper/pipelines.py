fa2d3eb61a65156eedd03f9239c1849ab7ec9727
Jackie Leng <jccleng@gmail.com>
2020-05-09 20:53:36 +0200

    use spider collection name

--- stock_scraper/pipelines.py
+++ stock_scraper/pipelines.py
@@ -13,7 +13,7 @@ class StockScraperPipeline:
 
 class MongoPipeline:
     def __init__(
-        self, mongo_uri: str, mongo_db: str, mongo_collection_name: str
+        self, mongo_uri: str, mongo_db: str, mongo_collection_name: str = None
     ):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
@@ -27,7 +27,7 @@ class MongoPipeline:
             ),
             mongo_db=crawler.settings.get("MONGO_DATABASE", "scrapy"),
             mongo_collection_name=crawler.settings.get(
-                "MONGO_COLLECTION_NAME", "scrapy_items"
+                "MONGO_COLLECTION_NAME"
             ),
         )
 
@@ -39,7 +39,12 @@ class MongoPipeline:
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[self.mongo_collection_name].replace_one(
+        coll_name = (
+            self.mongo_collection_name
+            if self.mongo_collection_name
+            else spider.name
+        )
+        self.db[coll_name].replace_one(
             {"ticker": item["ticker"]}, dict(item), upsert=True
         )
         return item
